70ff13421e8d2b3aadfab3996415e524e4ecab39
estet07 <estetaleksandr@gmail.com>
2023-04-18 22:13:09 +0600

    addFormikToast

--- src/pages/RegistrPage/RegistrPage.jsx
+++ src/pages/RegistrPage/RegistrPage.jsx
@@ -1,68 +1,92 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import styles from './registrpage.module.css'
 import Button from '@mui/material/Button';
 import { authServices } from '../../services/auth';
+import { useFormik } from 'formik';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const RegistrPage = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [name, setName] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')
+    // const [name, setName] = useState('')
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            username: ''
+        },
+        onSubmit: async (values) => {
+            
+            try {
+                const { data } = await registration(values)
+                console.log(data._doc);
+                toast('пользователь зарегестрирован')
+            }catch (err) {
+                toast("Email или Имя уже существует")
+            }
+        }
+    });
 
     const { registration } = authServices();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const newUser = {
-            username: name,
-            email,
-            password, 
-        };
-        try {
-            const { data } = await registration(newUser)
-            console.log(data._doc);
-        }catch (err) {
-            console.log(err.response.data);
-        }
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     const newUser = {
+    //         username: name,
+    //         email,
+    //         password, 
+    //     };
+    //     try {
+    //         const { data } = await registration(newUser)
+    //         console.log(data._doc);
+    //     }catch (err) {
+    //         console.log(err.response.data);
+    //     }
         
-    }
+    // }
  
     return (
         <section className={styles.wrapper}>
             <h1 className={styles.title}>
                 Регистрация
             </h1>
-            <form className={styles.form}onSubmit={handleSubmit}>
+            <form className={styles.form}onSubmit={formik.handleSubmit}>
                 <TextField 
                 label="Ваш Email" 
                 variant="filled"
-                type='email' 
+                type='email'
+                name='email' 
                 required
                 fullWidth
                 style={{ marginBottom: "20px"}}
-                value={email}
-                onChange={e => setEmail(e.target.value)}
+                value={formik.values.email}
+                onChange={formik.handleChange}
                 />
                 <TextField 
                 label="Ваше имя" 
                 variant="filled"
                 type='text' 
+                name='username' 
                 required
                 fullWidth
                 style={{ marginBottom: "20px"}}
-                value={name}
-                onChange={e => setName(e.target.value)}
+                value={formik.values.username}
+                onChange={formik.handleChange}
                 />
                 <TextField 
                 label="Ваш пароль" 
                 variant="filled"
                 type='password' 
+                name='password' 
                 required
                 fullWidth
                 style={{ marginBottom: "40px"}}
-                value={password}
-                onChange={e => setPassword(e.target.value)}
+                value={formik.values.password}
+                onChange={formik.handleChange}
                 />
                 <Button
                     fullWidth 
@@ -73,6 +97,7 @@ const RegistrPage = () => {
                     Зарегестрироваться
                 </Button>
             </form>
+            <ToastContainer />
         </section>
     );
 };
